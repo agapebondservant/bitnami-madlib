6712f4789ad8e4bbfd713adde7cdce2c03185c88
Tola Awofolu <oawofolu@pivotal.io>
2023-08-24 14:55:03 -0400

    Updated README

--- resources/scripts/generate-pgmadlib-baseimage.sh
+++ resources/scripts/generate-pgmadlib-baseimage.sh
@@ -6,7 +6,7 @@ cd resources/docker/1.x
 
 # docker build -t ${DATA_E2E_REGISTRY_USERNAME}/${DATA_E2E_PGMADLIB_IMAGE_NM}:${DATA_E2E_PGMADLIB_IMAGE_TAG} .
 
-docker pull madlib/postgres_11:jenkins
+docker pull madlib/${DATA_E2E_PGMADLIB_IMAGE_NM}:jenkins
 
 docker tag madlib/${DATA_E2E_PGMADLIB_IMAGE_NM}:jenkins ${DATA_E2E_REGISTRY_USERNAME}/${DATA_E2E_PGMADLIB_IMAGE_NM}:${DATA_E2E_PGMADLIB_IMAGE_TAG}
 
